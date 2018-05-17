c7caaa9cce8b34f97e9fb61bcba8a4e62f045c36
Rails App <pritesh@wow.com>
2018-05-17 12:23:12 +0000

    Added partials and destroy route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,6 @@
 class ArticlesController < ApplicationController
     def index
         @articles = Article.all
-
-
     end
 
     def new
@@ -35,6 +33,12 @@ class ArticlesController < ApplicationController
 
     @article = Article.find(params[:id])
 
+    end
+    def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
     end
 
     private
