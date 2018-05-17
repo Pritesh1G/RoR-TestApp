02c3a5a55eae3005d0af37c5074e0383dc952ff4
Rails App <pritesh@wow.com>
2018-05-17 15:29:12 +0000

    Added Bootstrap and styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require activestorage
 //= require_tree .
