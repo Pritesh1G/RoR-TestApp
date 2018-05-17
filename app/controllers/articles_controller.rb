4ea96a0c34ddef7718efda6498ab96107370cfeb
Rails App <pritesh@wow.com>
2018-05-17 12:05:54 +0000

    added edit update show and index listings

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,16 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+
+
+    end
+
     def new
     @article = Article.new
     end
-
+    def edit
+        @article = Article.find(params[:id])
+    end
     def create
      @article = Article.new(article_params)
         if @article.save
@@ -12,6 +20,17 @@ class ArticlesController < ApplicationController
         render 'new'
         end
     end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+        flash[:notice] = "Article was updated"
+        redirect_to article_path(@article)
+        else
+        render 'edit'
+        end
+    end
+
     def show
 
     @article = Article.find(params[:id])
