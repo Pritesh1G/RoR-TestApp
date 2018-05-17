eae01ea2ca5a19fc83c595ed64b5d3cd9de6a715
Rails App <pritesh@wow.com>
2018-05-17 11:47:49 +0000

    Added articles controller and modified routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,26 @@
 class ArticlesController < ApplicationController
-def new
+    def new
     @article = Article.new
+    end
 
-end
-def create
-    #render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
-end
-private
+    def create
+     @article = Article.new(article_params)
+        if @article.save
+        flash[:notice] = "Article was successfully created"
+        redirect_to article_path(@article)
+        else
+        render 'new'
+        end
+    end
+    def show
+
+    @article = Article.find(params[:id])
+
+    end
+
+    private
     def article_params
     params.require(:article).permit(:title, :description)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
 root 'welcome#home'
 get 'about', to: 'welcome#about'
-
 resources :articles
+
 end
