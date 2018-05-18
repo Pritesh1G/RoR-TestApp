135ef305c3e51189e678b06a3b27fcb9c1674dd6
Rails App <pritesh@wow.com>
2018-05-18 13:41:22 +0000

    Made the app look good

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,9 @@ class ArticlesController < ApplicationController
     end
     def create
      @article = Article.new(article_params)
+     @article.user = User.first
         if @article.save
-        flash[:notice] = "Article was successfully created"
+        flash[:success] = "Article was successfully created"
         redirect_to article_path(@article)
         else
         render 'new'
@@ -23,7 +24,7 @@ class ArticlesController < ApplicationController
 
     def update
         if @article.update(article_params)
-        flash[:notice] = "Article was updated"
+        flash[:success] = "Article was updated"
         redirect_to article_path(@article)
         else
         render 'edit'
@@ -36,7 +37,7 @@ class ArticlesController < ApplicationController
     end
     def destroy
     @article.destroy
-    flash[:notice] = "Article was deleted"
+    flash[:success] = "Article was deleted"
     redirect_to articles_path
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    has_many :articles
+    before_save { self.email = email.downcase }
     validates :username, presence: true,
                uniqueness: { case_sensitive: false },
                length: { minimum: 3, maximum: 25 }
@@ -6,5 +8,5 @@ class User < ActiveRecord::Base
     validates :email, presence: true,
                uniqueness: { case_sensitive: false },
                length: { maximum: 105 },
-               format: {width: VALID_EMAIL_REGEX }
+               format: {with: VALID_EMAIL_REGEX }
 end
